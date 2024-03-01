21c638d8cf61794934e0595ba25c49562fcccb04
HARI PRASAD S <hariprasad2k06@gmail.com>
2024-03-01 19:31:22 +0530

    added few other

---
+++ git-cheatsheet.py
@@ -0,0 +1,24 @@
+# Course link : https://www.coursera.org/learn/introduction-git-github/home/module/1
+# By : Google
+# Name : Git & Git-Hub
+
+
+# NOTE : VERSION CONTROL SYSTEM 
+# - KEEP'S HISTORICAL COPY OF CODE {They help us see what the project was looking before}
+# - Every small code-change is taken copy and tracked 
+# - Basically, Keeps track of changes made to our files.
+
+# NOTE IF YOU HAVE A SAME COPY OF TWO CODE HOW DO YOU CHECK THE DIFFERENCE ?
+
+# NOTE NOTE NOTE diff - compares two different files and show the difference 
+# diff file_name.py file_name.py
+
+# NOTE NOTE NOTE : diff -u file1 file2
+# shows what is added 
+# 
+
+# NOTE NOTE NOTE cat - to see what is in the file using CLI
+
+#
+
+
